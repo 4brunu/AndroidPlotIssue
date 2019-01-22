e8e1eb4b02f517dba5776b45fdfcf33a78347c4d
Bruno Coelho <4brunu@gmail.com>
2019-01-22 17:38:39 +0000

    Android - clean unnecessary stuff

--- app/src/main/java/com/example/androidplotissue/MainActivity.kt
+++ app/src/main/java/com/example/androidplotissue/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.androidplotissue
 
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.androidplot.xy.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +12,6 @@ class MainActivity : AppCompatActivity() {
 
         val plot = findViewById<XYPlot>(R.id.plot)
 
-        configurePlotAspect(plot)
-
-        configurePlotSeries(plot)
-
-    }
-
-    private fun configurePlotAspect(xyPlot: XYPlot) {
-
-        xyPlot.borderPaint.color = Color.TRANSPARENT
-        xyPlot.backgroundPaint.color = Color.TRANSPARENT
-
-        xyPlot.graphWidget.rangeGridLinePaint.color = Color.TRANSPARENT
-
-        xyPlot.graphWidget.domainOriginLinePaint.color = Color.TRANSPARENT
-        xyPlot.graphWidget.rangeOriginLinePaint.color = Color.TRANSPARENT
-
-    }
-
-    private fun configurePlotSeries(xyPlot: XYPlot) {
-
         val serie1Values = listOf<Number>(8, 4, 2, 8, 4, 16, 8, 25, 16, 8)
 
         val series1 = SimpleXYSeries(
@@ -42,15 +20,12 @@ class MainActivity : AppCompatActivity() {
             "Series1"
         )
 
-        xyPlot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, serie1Values.size.toDouble())
-        xyPlot.setRangeBoundaries(0, 30, BoundaryMode.FIXED)
-
-        xyPlot.graphWidget.setGridPadding(20f, 0f, 20f, 0f)
+        plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, serie1Values.size.toDouble())
+        plot.graphWidget.setGridPadding(20f, 0f, 20f, 0f)
 
         val series1Format = LineAndPointFormatter()
         series1Format.pointLabelFormatter = PointLabelFormatter()
 
-        // add a new series' to the xyplot:
         plot.addSeries(series1, series1Format)
 
     }
